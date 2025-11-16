a346e1d75d2d2b1e67611207aab4a496de0d84c7
JorgeCardenasY <jorge.cardenas.y@gmail.com>
2025-11-16 16:11:31 -0300

    portafolio

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -13,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("srv-d4d1fpggjchc73dn7uog")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,m8-ae2-abp.onrender.com,render.com').split(',')
 
